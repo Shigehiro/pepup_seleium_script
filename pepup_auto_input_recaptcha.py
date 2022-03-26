08f38400bf7a52c8d89eac52d3ef549718af2d16
Shigehiro Hattori <narihiro.h@gmail.com>
2022-03-27 01:10:41 +0900

    change the way of finding an element from by_css_selector to by_xpath

--- pepup_auto_input_recaptcha.py
+++ pepup_auto_input_recaptcha.py
@@ -24,120 +24,125 @@ options = webdriver.ChromeOptions()
 options.add_argument(f'service={chrome_service}')
 options.add_extension(EXTENSION_PATH)
 
-#driver = webdriver.Chrome(options=options, desired_capabilities=DesiredCapabilities.CHROME)
-
 # pass captcha and then login
 driver = webdriver.Chrome(options=options)
 driver.get('https://pepup.life/home')
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 driver.switch_to.frame(0)
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 driver.find_element(By.CSS_SELECTOR, ".recaptcha-checkbox-border").click()
-time.sleep(3)
+time.sleep(random.randint(3,6))
 
 driver.switch_to.default_content()
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 driver.switch_to.frame(2)
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 driver.find_element(By.CSS_SELECTOR, ".help-button-holder").click()
-time.sleep(10)
+time.sleep(random.randint(3,6))
 
 driver.switch_to.default_content()
 user = driver.find_element_by_id("sender-email")
 user.send_keys(args.user)
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 password = driver.find_element_by_id("user-pass")
 password.send_keys(args.password)
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 commit = driver.find_element_by_name("commit")
 commit.click()
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 # go to wakuwaku input page and submit today's data
 
 d = datetime.datetime.now() 
 date = d.strftime('%Y/X%m/X%d').replace('X0','X').replace('X','')
 driver.get('https://pepup.life/scsk_mileage_campaigns/%s' % date)
-time.sleep(3)
+time.sleep(random.randint(1,3))
 
 # walking
 random_step = random.randrange(10000,15000,100)
 step_count = driver.find_element_by_css_selector("div:nth-child(1) > .sc-1mklvxx-5")
 step_count.clear()
 step_count.send_keys(random_step)
-time.sleep(3)
+time.sleep(0.05)
 
 # sleep time
 sleep_time = driver.find_element_by_css_selector("div:nth-child(2) > .sc-1mklvxx-5")
 sleep_time.clear()
 sleep_time.send_keys("8")
-time.sleep(3)
+time.sleep(0.05)
 
 # sleep condition
-param3 = driver.find_element_by_css_selector("div:nth-child(3) > .sc-1mklvxx-6 .sc-1mklvxx-8")
-if param3.is_selected:
-    time.sleep(0.5)
+#param3 = driver.find_element_by_css_selector("div:nth-child(3) > .sc-1mklvxx-6 .sc-1mklvxx-8")
+param3 = driver.find_element_by_xpath("//label[contains(.,\'自分にとって良い睡眠習慣を実行する\')]")
+if param3.is_selected():
+    time.sleep(0.05)
 else:
     param3.click()
-    time.sleep(3)
+    time.sleep(0.05)
 
 # alcohol
-param4 = driver.find_element_by_css_selector("div:nth-child(4) > .sc-1mklvxx-6 > .sc-1mklvxx-7")
-if param4.is_selected:
-    time.sleep(0.5)
+#param4 = driver.find_element_by_css_selector("div:nth-child(4) > .sc-1mklvxx-6 > .sc-1mklvxx-7")
+param4 = driver.find_element_by_xpath("//label[contains(.,\'アルコール摂取20ｇ以内（休肝日含む）\')]")
+if param4.is_selected():
+    time.sleep(0.05)
 else:
     param4.click()
-    time.sleep(3)
+    time.sleep(0.05)
 
 # do not drink after dinner
-param5 = driver.find_element_by_css_selector("div:nth-child(5) > .sc-1mklvxx-6:nth-child(2) .sc-1mklvxx-8")
-if param5.is_selected:
-    time.sleep(0.5)
+#param5 = driver.find_element_by_css_selector("div:nth-child(5) > .sc-1mklvxx-6:nth-child(2) .sc-1mklvxx-8")
+param5 = driver.find_element_by_xpath("//label[contains(.,\'夕食（飲酒）後の夜食（締め）を食べない\')]")
+if param5.is_selected():
+    time.sleep(0.05)
 else:
     param5.click()
-    time.sleep(3)
+    time.sleep(0.05)
 
 # calory
-param6 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(3) .sc-1mklvxx-8")
-if param6.is_selected:
-    time.sleep(0.5)
+#param6 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(3) .sc-1mklvxx-8")
+param6 = driver.find_element_by_xpath("//label[contains(.,\'必要摂取カロリーを意識して1日の食事を組み立てる\')]")
+if param6.is_selected():
+    time.sleep(0.05)
 else:
     param6.click()
-    time.sleep(3)
+    time.sleep(0.05)
 
 # syusyoku, syusai, fukusai
-param7 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(4) .sc-1mklvxx-8")
-if param7.is_selected:
-    time.sleep(0.5)
+#param7 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(4) .sc-1mklvxx-8")
+param7 = driver.find_element_by_xpath("//label[contains(.,\'「主食」「主菜」「副菜」をバランスよく摂る\')]")
+if param7.is_selected():
+    time.sleep(0.05)
 else:
     param7.click()
-    time.sleep(3)
+    time.sleep(0.05)
 
 # do not eat snack between meal
-param8 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(5) .sc-1mklvxx-8")
-if param8.is_selected:
-    time.sleep(0.5)
+#param8 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(5) .sc-1mklvxx-8")
+param8 = driver.find_element_by_xpath("//label[contains(.,\'糖質を多く含む間食を控える\')]")
+if param8.is_selected():
+    time.sleep(0.05)
 else:
     param8.click()
-    time.sleep(3)
+    time.sleep(0.05)
 
 # eat breakfast
-param9 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(6) .sc-1mklvxx-8")
-if param9.is_selected:
-    time.sleep(0.5)
+#param9 = driver.find_element_by_css_selector(".sc-1mklvxx-6:nth-child(6) .sc-1mklvxx-8")
+param9 = driver.find_element_by_xpath("//label[contains(.,\'朝食を食べる\')]")
+if param9.is_selected():
+    time.sleep(0.05)
 else:
     param9.click()
-    time.sleep(3)
+    time.sleep(0.05)
 
 # submit
 submit = driver.find_element_by_css_selector(".sc-g7yrzm-7")
 submit.click()
-time.sleep(5)
+time.sleep(3)
 
 driver.quit()
